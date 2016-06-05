c2018f0585f484d22ac5d4d02d0696d8de80eccc
Armin <armin.bagheri.1380@gmail.com>
2016-06-05 11:10:39 +0430

    Add files via upload

---
+++ plugins/lockreply.lua
@@ -0,0 +1,41 @@
+do
+
+local function pre_process(msg)
+    
+    --Checking mute
+    local hash = 'mate:'..msg.to.id
+    if redis:get(hash) and msg.reply_id and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg) then
+            delete_msg(msg.id, ok_cb, true)
+            return "Done"
+        end
+    
+        return msg
+    end
+
+
+local function run(msg, matches)
+    chat_id =  msg.to.id
+    
+    if is_momod(msg) and matches[1] == 'lock' then
+      
+            
+                    local hash = 'mate:'..msg.to.id
+                    redis:set(hash, true)
+                    return "Reply Has Been Locked"
+  elseif is_momod(msg) and matches[1] == 'unlock' then
+                    local hash = 'mate:'..msg.to.id
+                    redis:del(hash)
+                    return "Reply Has Been UnLocked"
+end
+
+end
+
+return {
+    patterns = {
+        '^[/!#](lock) reply$',
+        '^[/!#](unlock) reply$'
+    },
+    run = run,
+    pre_process = pre_process
+}
+end
