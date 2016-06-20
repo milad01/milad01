eb7d2c5ca3b737a78543a56e3a367036070a3096
imashiam <ashkanpj.ap2@gmail.com>
2016-06-20 16:56:35 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,6 +1,6 @@
 do
 function run(msg, matches)
-  return " 💠TeleIran Tools Commands: \n\n\n 🚫Anti Words: \n #block [Word] \n #unblock [Word] \n\n 🎁Sticker Maker: \n #sticker \n #png \n\n 📷WebShot: \n #web [Site] \n\n 📚Calculator: \n #calc [Number] \n\n 🎤Text To Voice: \n #vc [Text] \n\n 🌦Weather: \n #weather [City] \n\n ⏰Online Clock: \n #time \n\n\n ✅Channel: @TeleIranTeam☢"
+  return " 💠Worldbot Tools Commands: \n\n\n 🚫Anti Words: \n #block [Word] \n #unblock [Word] \n\n 🎁Sticker Maker: \n #sticker \n #png \n\n 📷WebShot: \n #web [Site] \n\n 📚Calculator: \n #calc [Number] \n\n 🎤Text To Voice: \n #vc [Text] \n\n 🌦Weather: \n #weather [City] \n\n ⏰Online Clock: \n #time \n\n\n ✅Channel: https://telegram.me/joinchat/C9R_Yz6n71KH2zbWzK2cJg ☢"
   end
 return {
   patterns = {
