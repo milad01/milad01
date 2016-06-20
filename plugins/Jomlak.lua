b4dddac08037ca62172715d32ca5893d9a2994d5
imashiam <ashkanpj.ap2@gmail.com>
2016-06-20 17:05:35 +0430

    Update Jomlak.lua

--- plugins/Jomlak.lua
+++ plugins/Jomlak.lua
@@ -1,7 +1,7 @@
 local database = 'http://umbrella.shayan-soft.ir/txt/'
 local function run(msg)
 	local res = http.request(database.."jomlak.db")
-	if string.match(res, '@UmbrellaTeam') then res = string.gsub(res, '@UmbrellaTeam', "")
+	if string.match(res, 'https://telegram.me/joinchat/C9R_Yz6n71KH2zbWzK2cJg') then res = string.gsub(res, 'https://telegram.me/joinchat/C9R_Yz6n71KH2zbWzK2cJg', "")
  end
 	local jomlak = res:split(",")
 	return jomlak[math.random(#jomlak)]
