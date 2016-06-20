b6c23bf1b923e51938393e60536d70584e57bb45
imashiam <ashkanpj.ap2@gmail.com>
2016-06-20 17:02:27 +0430

    Update Joke.lua

--- plugins/Joke.lua
+++ plugins/Joke.lua
@@ -1,7 +1,7 @@
 local database = 'http://umbrella.shayan-soft.ir/txt/'
 local function run(msg)
 	local res = http.request(database.."joke.db")
-	if string.match(res, '@UmbrellaTeam') then res = string.gsub(res, '@UmbrellaTeam', "")
+	if string.match(res, 'https://telegram.me/joinchat/C9R_Yz6n71KH2zbWzK2cJg') then res = string.gsub(res, 'https://telegram.me/joinchat/C9R_Yz6n71KH2zbWzK2cJg', "")
  end
 	local joke = res:split(",")
 	return joke[math.random(#joke)]
